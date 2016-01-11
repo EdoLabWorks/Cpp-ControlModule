af3816d31adb94b3815cfd910fbdecd22688777f
Ed Alegrid <e14d@outlook.com>
2016-01-11 14:46:33 -0500

    using C++ error handler

---
+++ SocketError.h
@@ -0,0 +1,32 @@
+
+/*
+ * File:   SocketEror.h
+ * Author: Ed Alegrid
+ *
+ */
+
+#pragma once
+#include <errno.h>
+
+class SocketError : public std::exception
+{
+     std::string erMsg = "null";
+     char* ErrCode;
+
+     public:
+        // with no parameter, error message defaults to errno
+        SocketError() : ErrCode(strerror(errno)) {}
+        // enter your own descriptive error message
+        SocketError(const char* msg) : erMsg(msg) {}
+        virtual ~SocketError() {}
+        virtual const char* what() const noexcept override
+        {
+            if(erMsg == "null") {
+            return ErrCode;
+            }
+            else{
+            return erMsg.c_str();
+            }
+        }
+};
+
