7b307cfaa9aa7c48bfed923a8f74237257517fb3
Ed Alegrid <e14d@outlook.com>
2016-04-01 08:59:02 -0400

    added non-blocking read timeout

--- TcpClient.h
+++ TcpClient.h
@@ -1,4 +1,5 @@
 
+
 /*
  * File:   TcpClient.h
  * Author: Ed Alegrid
@@ -10,6 +11,7 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <errno.h>
+#include <sys/poll.h>
 #include "SocketError.h"
 
 namespace TcpClient {
@@ -17,27 +19,23 @@ using namespace std;
 
 class ClientSocket
 {
-      char* ip;
-      char* port;
-      int sockfd, rv;
-      char data[256];
-      struct addrinfo hints, *servinfo, *p;
-      char s[INET6_ADDRSTRLEN];
+      	int sockfd, rv, rd;
+      	char data[256];
+      	struct addrinfo hints, *servinfo, *p;
+      	char s[INET6_ADDRSTRLEN];
+      	struct pollfd rs[1]; //read socket array for timeout
 
-      void *get_addr(struct sockaddr *sa)
-      {
+      	void *get_addr(struct sockaddr *sa)
+      	{
             if (sa->sa_family == AF_INET) {
             return &(((struct sockaddr_in*)sa)->sin_addr);
             }
             return &(((struct sockaddr_in6*)sa)->sin6_addr);
-      }
+      	}
 
-      // initialize a TCP client socket and attempt to make a connection to a remote endpoint
-      const void initSocket(char* portno, char* clientip)
-      {
-            ip = clientip;
-            port = portno;
-
+      	// initialize a TCP client socket and attempt to make a connection to a remote endpoint
+      	const void initSocket(char* port, char* ip)
+      	{
             memset(&hints, 0, sizeof hints);
             hints.ai_family = AF_UNSPEC;
             hints.ai_socktype = SOCK_STREAM;
@@ -61,37 +59,41 @@ class ClientSocket
                      throw SocketError();
                 }
                 break;
-
-
-                if (p == NULL) {
-                    throw SocketError("client connect fail ...");
-                }
             }
+
+            if (p == NULL) {
+                throw SocketError("client connect fail ...");
+            }
+
             // details of remote connected endpoint
             inet_ntop(p->ai_family, get_addr((struct sockaddr *)p->ai_addr), s, sizeof s);
-            //cout << "client connected to: " << s << ": " << port << endl; //debug ouput
+            cout << "client connected to: " << s << ": " << port << endl; //debug ouput
             freeaddrinfo(servinfo);
-      }
+      	}
 
-      // cleanup method
-      void closeHandler() const
-      {
+      	// cleanup method
+      	void closeHandler() const
+      	{
             Close();
             delete this;
             exit(1);
-      }
+       	}
 
-      public:
+        public:
+        // 1st ctor, use with Connect() method
         ClientSocket() {}
-        ClientSocket(char* clientip, char* portno): port{portno}, ip{clientip} {}
+        // 2nd ctor, immediately initialize the client socket with the port and ip provided
+        // if no ip provided it will default to "localhost"
+        ClientSocket(char* port, char* ip = "localhost")  {initSocket(port, ip);}
         virtual ~ClientSocket() {}
 
-	// provide remote endpoint port no. and ip address 
-        virtual const void Connect(char* portno, char* clientip = "localhost")
+        // provide server port to connect with the listening server, use with 1st ctor
+        // if no ip provided it will default to "localhost"
+        virtual const void Connect(  char* port, char* ip = "localhost")
         {
             try
             {
-                initSocket(portno, clientip);
+                initSocket(port, ip);
             }
             catch (SocketError& e)
             {
@@ -105,9 +107,7 @@ class ClientSocket
         {
             try
             {
-                int len;
-                len = strlen(msg);
-                int n = send(sockfd, msg, len, 0);
+                int n = send(sockfd, msg, strlen(msg), 0);
                 if (n < 0) {
                     throw SocketError();
                 }
@@ -117,17 +117,36 @@ class ClientSocket
                  cerr << "Client Send Error: " << e.what() << endl;
                  closeHandler();
             }
-        }
+       }
 
        // read data
        virtual const char* Read()
        {
             try
             {
-                bzero(data, sizeof(data));
-                int n = read(sockfd, data, sizeof(data));
-                if (n < 0) {
+                rs[0].fd = sockfd;
+                // check for normal and out-of-bound data from the socket
+                rs[0].events = POLLIN | POLLPRI;
+                // check socket event for available data, wait 200 milliseconds for timeout
+                rd = poll(rs, 1, 200);
+                if (rd < 0) {
+                    //error("ERROR server reading from socket");
                     throw SocketError();
+                } else if (rd == 0) {
+                    cout << "Client read timeout error! No data received!\n";
+                } else {
+                    int n;
+                    bzero(data, sizeof(data));
+                    // check for events on newsockfd:
+                    if (rs[0].revents & POLLIN) {
+                        n = recv(sockfd, data, sizeof(data), 0); // receive normal data
+                    }
+                    if (rs[0].revents & POLLPRI) {
+                        n = recv(sockfd, data, sizeof(data), MSG_OOB); // out-of-band data
+                    }
+                    if (n == 0){
+                         cout << "Client read error, socket is closed or disconnected!\n";
+                    }
                 }
             }
             catch (SocketError& e)
@@ -147,3 +166,4 @@ class ClientSocket
 };
 
 }
+
