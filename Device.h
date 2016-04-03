b392c42a88605649ffa808ceacf0923e4d132e86
Ed Alegrid <e14d@outlook.com>
2016-04-03 11:49:23 -0400

    refactor using string data types

--- Device.h
+++ Device.h
@@ -1,10 +1,6 @@
+*
+*/
 
-// -*- C++ -*-
-/*
- * File:   Device.h
- * Author: Ed Alegrid
- *
- */
 #pragma once
 
 namespace Device {
@@ -17,50 +13,50 @@ class ControlLogic
         ~ControlLogic() {}
 
         // control code operation
-        const char* processData(const char message[256])
+        const string processData(string m)
         {
-            string RxData = message;
-            const char* data;
+            string data;
+
             // device1
-            if (RxData == "ON1"){
+            if (m == "ON1"){
                 data = "Device1 is ON";
             }
-            else if(RxData == "OFF1"){
+            else if(m == "OFF1"){
                 data = "Device1 is OFF";
             }
             // device2
-            else if (RxData == "ON2"){
+            else if (m == "ON2"){
                 data = "Device2 is ON";
             }
-            else if(RxData == "OFF2"){
+            else if(m == "OFF2"){
                 data = "Device2 is OFF";
             }
             // device3
-            else if (RxData == "ON3"){
+            else if (m == "ON3"){
                 data = "Device3 is ON";
             }
-            else if(RxData == "OFF3"){
+            else if(m == "OFF3"){
                 data = "Device3 is OFF";
             }
             // device4
-            else if (RxData == "ON4"){
+            else if (m == "ON4"){
                   data = "Device4 is ON";
             }
-            else if(RxData == "OFF4"){
+            else if(m == "OFF4"){
                   data = "Device4 is OFF";
             }
             // device5
-            else if (RxData == "ON5"){
+            else if (m == "ON5"){
                 data = "Device5 is ON";
             }
-            else if(RxData == "OFF5"){
+            else if(m == "OFF5"){
                 data = "Device5 is OFF";
             }
             // device6
-            else if (RxData == "ON6"){
+            else if (m == "ON6"){
                   data = "Device6 is ON";
             }
-            else if(RxData == "OFF6"){
+            else if(m == "OFF6"){
                   data = "Device6 is OFF";
             }
             else
@@ -68,7 +64,7 @@ class ControlLogic
 
             return data;
         }
-
 };
 
 }
+
