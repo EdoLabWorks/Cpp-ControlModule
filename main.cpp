798c58e5dab814d7d0fbceae7531717bfce96ac1
Ed Alegrid <e14d@outlook.com>
2016-04-03 11:25:18 -0400

    added more demo samples

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
-
 /*
- * File:   main.ccp
- * Author: Ed Alegrid
- *
- * For now, this app will work only on Linux OS.
- * Use any C++11 compliant compiler.
- * This is a simple synchronous TCP client/server process with non-blocking read timeout.
- * Mainly use for testing TCP socket communication with Node-WebControl Project.
- * 
- */
+* File:   main.ccp
+* Author: Ed Alegrid
+*
+* For now, this app will work only on Linux OS.
+* Use any C++11 compliant compiler or IDE. I have used it with CodeBlocks and NetBeans w/o problems.
+* I always set it to emit all compiler warnings and try to resolve each one by one.
+* There are still plenty of warnings!
+*
+* 
+* This is a very simple synchronous TCP client/server library with non-blocking read timeout.
+* It was mainly use for testing TCP socket communications with Node-WebControl project.
+* 
+*/
 
 #include <iostream>
 #include <memory>
@@ -20,7 +23,13 @@ int main()
 {
 
     unique_ptr<Controller::App> app(new Controller::App);
+
+    /* run and test each method one at a time */
+
     app->startCtrl();
+    //app->startTest();
+    //app->startOtherTest();
+    //app->startEchoServer();
 
    return 0;
 }
