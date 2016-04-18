1ad568a4ca8bb5f983b3a4f86d0b7ac769825926
Ed Alegrid <e14d@outlook.com>
2016-04-17 20:27:18 -0400

    refactor for shared pointer

--- app/controller.h
+++ app/controller.h
@@ -11,9 +11,7 @@
 #include "../tcp/client.h"
 #include "device.h"
 
-namespace Controller {
 using namespace std;
-
 class App
 {
     public:
@@ -22,19 +20,22 @@ class App
 
         /* all application logic goes here */
 
-	/********************
-    * webcontrol server *
-	*********************/
+        /********************
+    	* webcontrol server *
+        *********************/
         void startCtrl()
         {
             cout << "\n*** C++ IO-Control Project ***\n" << endl;
 
             int serverport = 51111;
             int clientport = 5555;
+
             // create server, ctrlLogic and client
             unique_ptr<Tcp::Server> server(new Tcp::Server);
-            unique_ptr<Device:: ControlLogic> ControlModule(new  Device::ControlLogic);
-            unique_ptr<Tcp::Client> client(new Tcp::Client);
+            //auto server = make_unique<Tcp::Server>(); //C++14
+
+            auto device = make_shared<Device::Operation>(); //cm ControlModule
+            auto client = make_shared<Tcp::Client>();
 
             server->createServer(serverport);
             int loop = true;
@@ -49,7 +50,7 @@ class App
                     string data = server->Read();
                     cout << "data from web client: " << data << endl;
                     // process received data
-                    string msg = ControlModule->processData(data);
+                    string msg = device->processData(data);
                     cout << "device status: " << msg << endl;
                     // tcp client connecting to server
                     // provide remote endpoint port and ip
@@ -71,43 +72,41 @@ class App
                     cerr << "error: " << e.what() << endl;
                     cout << "Please restart server." << endl;
                     server.reset(nullptr);
-                    exit(1);
+
                 }
             }
          server.reset(nullptr);
-         exit(1);
+
         }
 
-	/*************************************************************************
-    * a quick client/server communication and non-blocking read timeout test *
-	**************************************************************************/
+        /*************************************************************************
+    	* a quick client/server communication and non-blocking read timeout test *
+        **************************************************************************/
          void startTest()
         {
             cout << "\n*** C++ IO-Control Test ***\n" << endl;
 
             try
             {
-                unique_ptr<Tcp::Server> server(new Tcp::Server(51111));
-                unique_ptr<Tcp::Client> client(new Tcp::Client(51111));
-                server->Listen();
+            	auto server = make_shared<Tcp::Server>(51111);
+            	auto client = make_shared<Tcp::Client>(51111);
+            	server->Listen();
 
-                // client read should timeout since server didn't send yet any msg
-                // the process should continue on the next operation
+            	// client read should timeout since server didn't send yet any msg
+            	// the process should continue on the next operation
                 client->Read(); //non-blocking client read timeout
 
                 client->Send("Yo server, hi ya!");
-                    cout << "data from client: " << server->Read() << endl;
+                cout << "data from client: " << server->Read() << endl;
                 server->Send("Client bro, what's up!");
-                    cout << "msg from server: " << client->Read() << endl;
+                cout << "msg from server: " << client->Read() << endl;
                 client->Send("Server bro, I'm doin great!");
-                    cout << "data from client: " << server->Read() << endl;
-
+                cout << "data from client: " << server->Read() << endl;
                 // server read should timeout since client didn't send data after the first one
                 server->Read(); //non-blocking server read timeout
 
                 server->Send("You're a lucky dude! See yah later, client bro!");
-                    cout << "msg from server: " << client->Read() << endl;
-
+                cout << "msg from server: " << client->Read() << endl;
                 client->Close();
                 // since Listen() is set to one time use w/o parameter
                 // close operation will close both newsockfd and sockfd and stop the process
@@ -117,19 +116,16 @@ class App
             catch (SocketError& e)
             {
                 cerr << "error: " << e.what() << endl;
-                exit(1);
-            }
 
-        exit(0);
+            }
         }
 
-	/**************************
-    * test using stack memory *
-	***************************/
+        /**************************
+    	* test using stack memory *
+        ***************************/
         void startOtherTest()
         {
             cout << "\n*** C++ IO-Control Test using stack memory ***\n" << endl;
-
             using namespace Tcp;
 
             try
@@ -157,21 +153,19 @@ class App
             catch (SocketError& e)
             {
                 cerr << "error: " << e.what() << endl;
-                exit(1);
+
             }
 
-        exit(0);
         }
 
-	/****************************
-    * a simple echo server demo *
-	*****************************/
+        /****************************
+    	* a simple echo server demo *
+        *****************************/
         void startEchoServer()
         {
             cout << "\n*** C++ IO-Control Echo Server Demo ***\n" << endl;
 
-            unique_ptr<Tcp::Server> s(new Tcp::Server(51111));
-
+            auto s = make_shared<Tcp::Server>(51111);
             for (;;)
             {
                 s->Listen(true);
@@ -182,4 +176,3 @@ class App
         }
 };
 
-}
