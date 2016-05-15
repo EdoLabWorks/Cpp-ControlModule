0a0833a53b56408f50d4a652cab66515fe0e73c4
Ed Alegrid <e14d@outlook.com>
2016-05-15 11:47:48 -0400

    refactor for using static method

--- main.cpp
+++ main.cpp
@@ -4,25 +4,22 @@
 *
 * A simple TCP client/server library with non-blocking read/send operations.
 * It was mainly use for testing TCP socket communications with Node-WebControl project.
-* 
-* Use any Linux C++11 compliant compiler or IDE. 
+*
+* Use any Linux C++11 compliant compiler or IDE.
 *
 */
 
 #include <iostream>
 #include <memory>
 #include "app/controller.h"
+#include "test/test.h"
 
 using namespace std;
 
 int main()
 {
-    unique_ptr<App> app(new App);
-
-    app->startCtrl();
-    //app->startTest();
-    //app->startOtherTest();
-    //app->startEchoServer();
+   App::startCtrl();
+   //Test::startAll();
 
    return 0;
 }
